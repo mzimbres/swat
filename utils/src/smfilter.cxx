85f67383c4916e468abeaf0422cb9194f870775d
Marcelo zimbres <mzimbres@gmail.com>
2011-10-30 10:58:44 -0200

    Program smfilter with new options.

--- utils/src/smfilter.cxx
+++ utils/src/smfilter.cxx
@@ -17,12 +17,12 @@
  */
 
 #include <iostream>
-
-// ROOT
+#include <memory>
 
 // SWAT
 
 #include "TAlm.h"
+#include "TVMap.h"
 #include "TAuxFunc.h"
 #include "TParser.h"
 
@@ -33,13 +33,6 @@ void print_usage(const char* progname);
 
 int main(int argc,const char* argv[])
 {
-   // The program reads TAlm object in file(first 
-   // argument) and filters using thresholds saved 
-   // in file(second argument). Filtered object 
-   // will be output in filteredNN.root file.
-
-   // Possible options
-
    TParser parser(argc,argv);
 
    string help("--help");
@@ -54,37 +47,22 @@ int main(int argc,const char* argv[])
       //cout << "Using J = 6" << endl;
    }
 
-   TAlm *alm = TAuxFunc::rand_gaus_alm(J,0,1);
+   auto_ptr<TAlm> alm(TAuxFunc::rand_gaus_alm(J,1,0.5));
 
    int N = 3; 
-   string bandlim("--wav-band-lim");
-   if (!parser.get<int>(N,bandlim)) {
-      //cout << "Using N = " << N << endl;
+   string bandlim("-N");
+   if (parser.get<int>(N,bandlim)) {
+      auto_ptr<TAlm> filtered(alm2wav2alm(*alm.get(),N,0,J));
+      if (!compare_alm(*alm.get(),*filtered.get()))
+         return 1;
+   } else {
+      TAlm alm2(*alm.get());
+      auto_ptr<TVMap> tot(TAuxFunc::SHT(alm2));
+      tot->CreateAlm(*alm.get());
+      if (!compare_alm(*alm.get(),alm2))
+         return 1;
    }
 
-   int ji = 0; 
-   string jmin("--jmin");
-   if (!parser.get<int>(ji,jmin)) {
-      //cout << "No jmin specified, using 0" << endl;
-   }
-
-   int jm = J; 
-   string jmax("--jmax");
-   if (!parser.get<int>(jm,jmax)) {
-      //cout << "No jmax specified, using J = " << J << endl;
-   }
-
-   TAlm *filtered = alm2wav2alm(*alm,N,ji,jm);
-   if (filtered == NULL){
-     cerr << "Unable to do the transform." << endl;
-     return 1;
-   } 
-
-   if (!compare_alm(*alm,*filtered))
-      return 1;
-
-   delete filtered;
-   delete alm;
    return 0;
 }
 
@@ -93,17 +71,14 @@ void print_usage(const char* progname)
    cout << "\n";
    cout << "   Tests the algorithm doing a forward an backward transform.\n";
    cout << "\n";
-   cout << "   Usage: " << progname 
-        << " --signal=file1 --thresholds=file2 --wav-band-lim=N --output-file=file3\n";
+   cout << "   Usage: " << progname << endl;
    cout << "   Options:\n";
    cout << "\n";
-   cout << "   --help:         This menu.\n";
-   cout << "   -J:             TAlm object in root file. If not provided, the program \n"
-        << "                   will compare input and output for J = 7 and return true if\n"
-	<< "                   diffrence less that 10e-10. \n";
-   cout << "   --wav-band-lim: Band limit of wavelet to be used, defaults to N = 3.\n";
-   cout << "   --jmin:         Will begin the analysis at this scale, defaults to 0.\n";
-   cout << "   --jmax:         Will end the analysis at this scale, defaults to alm.GetJmax().\n";
+   cout << "   --help: This menu.\n";
+   cout << "   -J:     TAlm object in root file. If not provided, the program \n"
+        << "           will compare input and output for J = 7 and return true if\n"
+	<< "           diffrence less that 10e-10. \n";
+   cout << "   -N:     Band limit of wavelet to be used.\n";
    cout << "\n";
 }
 
